272aee2bb463e69f97a6baad002e6dfa510b3b8f
chaerimjo-web <chaerim010710@gmail.com>
2025-02-24 21:03:25 +0900

    키 메소드

---
+++ ggang_game/coding/code/js/class.js
@@ -0,0 +1,32 @@
+class Hero{
+	constructor(el){
+		this.el = document.querySelector(el);
+		this.movex = 0;
+		this.speed = 16; //변수선언
+	}
+	keyMotion(){
+		/* keydown오브젝트로 키눌림을 체크 후 조건문을 만든다. */
+		if(key.keyDown['left']){
+			this.el.classList.add('run');
+			this.el.classList.add('flip');
+
+			this.movex = this.movex - this.speed;
+		}else if(key.keyDown['right']){
+			this.el.classList.add('run');
+			this.el.classList.remove('flip');
+			
+			this.movex = this.movex + this.speed;
+		}
+		
+		if(!key.keyDown['left'] && !key.keyDown['right']){
+			this.el.classList.remove('run');
+		}
+		if(key.keyDown['x_key']){
+			this.el.classList.add('attack');
+		}
+		if(!key.keyDown['x_key']){
+			this.el.classList.remove('attack');
+		}
+		this.el.parentNode.style.transform = `translateX(${this.movex}px)`;
+	}
+}
--- ggang_game/coding/code/js/game.js
+++ ggang_game/coding/code/js/game.js
@@ -16,23 +16,44 @@ const key = {
 		37: 'left',
 		39: 'right',
 		38: 'up',
-		40: 'down'
+		40: 'down',
+		88: 'x_key'
 	}
 }
+
+const renderGame = () => {
+		window.requestAnimationFrame(renderGame); //재귀함수/ 자기자신을 호출 
+		hero.keyMotion(); //키를 누를때 key메소드를 호출한다. 		
+	}
 
 const windowEvent = () => {
 	window.addEventListener('keydown', e => {
 		key.keyDown[key.keyValue[e.which]] = true;
-		console.log(key.keyDown);
 	});
 	window.addEventListener('keyup', e => {
 		key.keyDown[key.keyValue[e.which]] = false;
-		console.log(key.keyDown);
 	})
+}
+let hero;
+
+const loadImg = () => {
+	const preLoadImgSrc = ['../../lib/images/ninja_run.png', '../../lib/images/ninja_attack.png']; //미리 로드할 이미지 경로들의 배열 
+	preLoadImgSrc.forEach(arr => {
+		const img = new Image(); //새로운 객체 생성 | new Image()로 img 객체를 만들었을 때 → 빈 <img> 태그를 만든 것과 같음.
+		img.src = arr; 
+		/*
+			img.src는 이미지의 경로(URL) 를 설정하는 속성
+			src 속성에 값을 넣으면 브라우저가 자동으로 해당 이미지를 로드
+			여기서 arr은 preLoadImgSrc 배열에 있는 문자열('../../lib/images/ninja_run.png') 
+		*/
+	});
 }
 
 const init = () => {
+	hero = new Hero('.hero'); //hero 인스턴스 호출
+	loadImg();
 	windowEvent();
+	renderGame();
 }
 
 window.onload = () => {
